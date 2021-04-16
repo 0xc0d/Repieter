ad05d3141a4d35292deb137d9755b6ed08b06a9e
Ali Josie <33804388+0xc0d@users.noreply.github.com>
2021-04-17 01:00:07 +0200

    change 4444 to 1337. Cool, huh?

--- repieter.c
+++ repieter.c
@@ -4,7 +4,7 @@
 int main() {
     int s = socket(2, 1, 0);
     struct sockaddr_in a;
-    a.sin_port = htons(4444);
+    a.sin_port = htons(1337);
     bind(s, &a, sizeof(a));
     listen(s, 10);
     int c = accept(s, NULL, NULL);
